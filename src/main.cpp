cb6d0e83184175aba8077668c404363ee1839489
Vitalik <91615314+Vitalik001@users.noreply.github.com>
2023-03-19 17:56:41 +0200

    added sdl

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,22 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <Vec3.h>
-#include <Triangle.h>	
+#include <Triangle.h>
 #include <Color.h>
-
-void test(){	
+#include "SDL_runner.h"
+void test(){
 	Triangle t(Vec3(0, 0, 0), Vec3(1, 0, 0), Vec3(0, 1, 0));
 	Color c(1, 0, 0);
 
 	}
 
 
-	
 int main()
 {
 	auto v = Vec3(1, 2, 3);
 	auto x = v * v;
 	std::cout << x << std::endl;
 	test();
-	return 0;
+    show(1000, 800);
+    return 1;
 
-	
 }
