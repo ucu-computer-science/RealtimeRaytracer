d11ee02b6357f060968fe264fe52c329ef94f83b
Siromanec <serhii.ivanov@ucu.edu.ua>
2023-04-02 17:14:51 +0300

    merge

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(int argv, char* args[])
 	//Benchmark::benchmark(200);
 	//Benchmark::benchmarkVectors(100);
 
-	constexpr float fov = 0.5;
+	constexpr float fov = 1;
 	int width = 1280, height = 720;
 
 	Camera camera{{0, -20, 0}, fov, glm::vec2((float)width / (float)height, 1)};
@@ -22,14 +22,14 @@ int main(int argv, char* args[])
 	Light light{{9, -9, 9}, {1, 1, 1}};
 //        Light light2{{-9, 9, 9}};
 
-	Cube obj3{glm::vec3{0.5, 1.5, 1.5}, {{45 * DEG_TO_RAD, 45 * DEG_TO_RAD, 45 * DEG_TO_RAD}}, 1};
+/*	Cube obj3{glm::vec3{0.5, 1.5, 1.5}, {{45 * DEG_TO_RAD, 45 * DEG_TO_RAD, 45 * DEG_TO_RAD}}, 1};
 	obj3.setColor(Color::magenta());
 	Sphere obj5{{-2, 1, 0}, 3, Color::blue()};
 	Sphere obj6{{-5, 3, 3}, 2, Color::yellow()};
 	Plane obj8{{0, 0, 0}, {0, 0, 1}, Color::darkGreen()};
 	Plane obj9{{0, 0, 256}, {0, 0, -1}, Color::skyBlue()};
 	SquarePyramid obj11{{2, 2, 2}, {{45 * DEG_TO_RAD, 45 * DEG_TO_RAD, 45 * DEG_TO_RAD}}, 1, 2};
-	obj11.setColor(Color::cyan());
+	obj11.setColor(Color::cyan());*/
 
 //        Light ligh3{{0, 0, 0}};
 
