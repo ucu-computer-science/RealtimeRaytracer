d9d00e9150c44c938c0383776e96d1e8901ff444
Siromanec <serhii.ivanov@ucu.edu.ua>
2023-04-02 16:21:13 +0300

    added presentation scene

--- src/main.cpp
+++ src/main.cpp
@@ -13,25 +13,38 @@ int main(int argv, char* args[])
 	//Benchmark::benchmark(200);
 	//Benchmark::benchmarkVectors(100);
 
-	constexpr float fov = 1;
+	constexpr float fov = 0.5;
 	int width = 1280, height = 720;
 
-	Camera camera{{0.5, 0, 0.5}, fov, glm::vec2((float)width / (float)height, 1)};
+	Camera camera{{0, -20, 0}, fov, glm::vec2((float)width / (float)height, 1)};
 	camera.setBackgroundColor(Color::gray());
 
-	Light light{{0, 0, 10}};
+	Light light{{9, -9, 9}};
+//        Light light2{{-9, 9, 9}};
 
-	Cube obj3{glm::vec3{0.5, 1.5, 1.5}, {{45 * DEG_TO_RAD, 45 * DEG_TO_RAD, 45 * DEG_TO_RAD}}, 1};
-	obj3.setColor(Color::magenta());
-	Sphere obj5{{-2, 1, 0}, 3, Color::blue()};
-	Sphere obj6{{-5, 3, 3}, 2, Color::yellow()};
-	Plane obj8{{0, 0, 0}, {0, 0, 1}, Color::darkGreen()};
-	Plane obj9{{0, 0, 256}, {0, 0, -1}, Color::skyBlue()};
-	SquarePyramid obj11{{2, 2, 2}, {{45 * DEG_TO_RAD, 45 * DEG_TO_RAD, 45 * DEG_TO_RAD}}, 1, 2};
-	obj11.setColor(Color::cyan());
+//        Light light3{{0, -9, 9}};
+
+//        Light ligh3{{0, 0, 0}};
+
+//	Cube obj3{glm::vec3{0.5, 1.5, 1.5}, {{45 * DEG_TO_RAD, 45 * DEG_TO_RAD, 45 * DEG_TO_RAD}}, 1};
+//	obj3.setColor(Color::magenta());
+//	Sphere obj5{{-2, 1, 0}, 3, Color::blue()};
+//	Sphere obj6{{-5, 3, 3}, 2, Color::yellow()};
+//	Plane obj8{{0, 0, 0}, {0, 0, 1}, Color::darkGreen()};
+//	Plane obj9{{0, 0, 256}, {0, 0, -1}, Color::skyBlue()};
+//	SquarePyramid obj11{{2, 2, 2}, {{45 * DEG_TO_RAD, 45 * DEG_TO_RAD, 45 * DEG_TO_RAD}}, 1, 2};
+//	obj11.setColor(Color::cyan());
+        Plane plane1{{-10, 0, 0}, {1, 0, 0}, Color::red()};
+        Plane plane2{{10, 0, 0}, {-1, 0, 0}, Color::green()};
+        Plane plane3{{0, 0, 10}, {0, 0, -1}, Color::white()};
+        Plane plane4{{0, 0, -10}, {0, 0, 1}, Color::white()};
+        Plane plane5{{0, 10, 0}, {0, -1, 0}, Color::white()};
+        Plane plane6{{0, -10, 0}, {0, 1, 0}, Color::white()};
+        Cube cube1{glm::vec3{4, -5, -6.7}, {{0 * DEG_TO_RAD, 0 * DEG_TO_RAD, -15 * DEG_TO_RAD}}, 6.6};
+        Sphere sphere1{{-4, 2, -5}, 36, Color::blue()};
+//        Camera::instance->skip = 2;
 
 	SDLDisplayer::display(width, height);
-	//Camera::instance->skip = 2;
 
 	return 0;
 }
